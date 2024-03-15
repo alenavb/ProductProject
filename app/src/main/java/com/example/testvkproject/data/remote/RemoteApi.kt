c4cef54be50e70a40febbef001e6dbebe3636c42
alena <direct.pavlova@yandex.ru>
2024-03-15 18:46:32 +0400

    add search feature

--- app/src/main/java/com/example/testvkproject/data/remote/RemoteApi.kt
+++ app/src/main/java/com/example/testvkproject/data/remote/RemoteApi.kt
@@ -10,4 +10,7 @@ interface RemoteApi {
     @GET("product")
     suspend fun getAllProduct(@Query("skip") skip: Int, @Query("limit") limit: Int = 20): Response<ModelProduct>
 
+    @GET("products/search")
+    suspend fun searchByTitle(@Query("q") query: String): Response<ModelProduct>
+
 }
